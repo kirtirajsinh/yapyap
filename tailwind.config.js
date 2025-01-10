47092f67b9702f1a59dadddd5a0528cea7414242
kirtirajsinh <kirtiraj3738@gmail.com>
2025-01-10 10:19:02 +0530

    Add components.json configuration and enhance tailwind.config.js with new theme settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,85 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    darkMode: ['class'],
+    content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        inter: ['Inter', 'sans-serif'],
-      },
-      colors: {
-        lobby: ' #121212',
-        audio: '#050505',
-        custom: {
-          1: ' #23262F',
-          2: '#121214',
-          3: '#181A20',
-          4: '#334155',
-          5: '#CBD5E1',
-          6: '#94A3B8',
-          7: '#E2E8F0',
-          8: '#246BFD',
-        },
-        rgbColors: {
-          1: 'rgba(24, 24, 27, 0.8)',
-          2: 'rgba(71, 85, 105, 0.2)',
-          3: 'rgba(148, 163, 184, 1)',
-          4: 'rgba(249, 112, 102, 0.1)',
-        },
-      },
-    },
+  	extend: {
+  		fontFamily: {
+  			inter: [
+  				'Inter',
+  				'sans-serif'
+  			]
+  		},
+  		colors: {
+  			lobby: ' #121212',
+  			audio: '#050505',
+  			custom: {
+  				'1': ' #23262F',
+  				'2': '#121214',
+  				'3': '#181A20',
+  				'4': '#334155',
+  				'5': '#CBD5E1',
+  				'6': '#94A3B8',
+  				'7': '#E2E8F0',
+  				'8': '#246BFD'
+  			},
+  			rgbColors: {
+  				'1': 'rgba(24, 24, 27, 0.8)',
+  				'2': 'rgba(71, 85, 105, 0.2)',
+  				'3': 'rgba(148, 163, 184, 1)',
+  				'4': 'rgba(249, 112, 102, 0.1)'
+  			},
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
