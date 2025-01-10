5dee2fc4b9a3088ce0228c75138f4bf646bf447d
kirtirajsinh <kirtiraj3738@gmail.com>
2025-01-10 10:22:29 +0530

    Replace IntroPage with CreateRoom component in Home function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,11 @@
-'use server';
-import IntroPage from '@/components/IntroPage/IntroPage';
-
-interface RoomDetails {
-  message: string;
-  data: {
-    roomId: string;
-  };
-}
-
-const createRandomRoom = async () => {
-  const res = await fetch('https://api.huddle01.com/api/v1/create-room', {
-    method: 'POST',
-    body: JSON.stringify({
-      title: 'Test Room',
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-      'x-api-key': process.env.API_KEY ?? '',
-    },
-    cache: 'no-store',
-  });
-  const data: RoomDetails = await res.json();
-  const { roomId } = data.data;
-  return roomId;
-};
+import CreateRoom from "@/components/Room/CreateRoom";
 
 export default async function Home() {
-  const roomId = await createRandomRoom();
-  return <IntroPage roomId={roomId} />;
+  // const roomId = await createRandomRoom();
+  // return <IntroPage roomId={roomId} />;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen ">
+      <CreateRoom />
+    </div>
+  );
 }
