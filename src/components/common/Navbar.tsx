dac22c02db24a27a8228335a017adc94e4587b4c
kirtirajsinh <kirtiraj3738@gmail.com>
2025-02-01 16:38:03 +0530

    Add WalletConnect component to Navbar for improved wallet integration

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import type React from "react";
+import WalletConnect from "../wallet/WalletConnect";
 // import Image from 'next/image';
 
 type NavbarProps = {};
@@ -8,16 +9,8 @@ type NavbarProps = {};
 const Navbar: React.FC<NavbarProps> = () => {
   return (
     <header className="border-b border-custom-1 w-full absolute top-0 left-0 h-16 flex items-center px-10 z-10 text-slate-100 justify-between">
-      {/* <Image
-        src="/images/Logo.png"
-        alt="logo"
-        width={180}
-        height={180}
-        className="object-contain"
-        quality={100}
-        priority
-      /> */}
       <h1 className="text-2xl font-bold md:text-4xl">Yapster</h1>
+      <WalletConnect />
     </header>
   );
 };
