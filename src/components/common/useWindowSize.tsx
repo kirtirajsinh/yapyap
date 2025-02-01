2afd9c934640a090698002efab5a20d519efb743
kirtirajsinh <kirtiraj3738@gmail.com>
2025-02-01 16:36:29 +0530

    Add custom hooks for device detection and window size management

---
+++ src/components/common/useWindowSize.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
+const useWindowSize = () => {
+  const [windowHeight, setWindowHeight] = useState(
+    typeof window !== "undefined" ? window.innerHeight : 0
+  );
+  const [windowWidth, setWindowWidth] = useState(
+    typeof window !== "undefined" ? window.innerWidth : 0
+  );
+
+  const onResize = () => {
+    setWindowHeight(window.innerHeight);
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    onResize();
+    window.addEventListener("resize", onResize);
+    return () => window.removeEventListener("resize", onResize);
+  }, []);
+
+  return [windowHeight, windowWidth];
+};
+
+export default useWindowSize;
