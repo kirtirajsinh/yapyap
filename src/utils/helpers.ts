8a3ee258168e48242704063be5d70ce1dea9cd6c
kirtirajsinh <kirtiraj3738@gmail.com>
2025-02-11 19:46:02 +0530

    Add geRoomMetaData function to fetch room metadata from API

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -8,3 +8,25 @@ export const cn = (...inputs: ClassValue[]) => {
 export const getFallbackAvatar = () => {
   return '/avatars/avatars/0.png';
 };
+
+export const geRoomMetaData = async (roomId: string) => {
+
+
+
+  const response = await fetch(`https://api.huddle01.com/api/v2/sdk/rooms/room-details/${roomId}`, {
+    method: "GET",
+    headers: {
+      'x-api-key': process.env.API_KEY || '',
+    },
+  }
+  )
+
+  if (!response.ok) {
+    console.log("Error getting room metadata:", response);
+    throw new Error(`Error getting room metadata: ${response.status} ${response.statusText}`);
+  }
+  const data = await response.json();
+  console.log("data from getting room metadata", data);
+  return data;
+}
+
