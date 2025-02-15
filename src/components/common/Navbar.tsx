6058d25ae431263017f91c4e4c532c2f52eeb5aa
kirtirajsinh <kirtiraj3738@gmail.com>
2025-02-15 11:34:55 +0530

    Replace site title with a link in Navbar component for improved navigation

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import type React from "react";
 import WalletConnect from "../wallet/WalletConnect";
+import Link from "next/link";
 // import Image from 'next/image';
 
 type NavbarProps = {};
@@ -9,7 +10,9 @@ type NavbarProps = {};
 const Navbar: React.FC<NavbarProps> = () => {
   return (
     <header className="border-b border-custom-1 w-full absolute top-0 left-0 h-16 flex items-center px-10 z-10 text-slate-100 justify-between">
-      <h1 className="text-2xl font-bold md:text-4xl">Yapster</h1>
+      <Link href="/" className="text-2xl font-bold md:text-4xl cursor-pointer">
+        Yapster
+      </Link>
       <WalletConnect />
     </header>
   );
