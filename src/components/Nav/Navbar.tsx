991a584e5504ac1b03e00c116839830698f19d9f
kirtirajsinh <kirtiraj3738@gmail.com>
2025-02-19 18:32:10 +0530

    Add Navbar and AddFrame components for enhanced navigation and user interaction

--- src/components/Nav/Navbar.tsx
+++ src/components/Nav/Navbar.tsx
@@ -1,16 +1,25 @@
 "use client";
 
 import type React from "react";
+import AddFrame from "../common/AddFrame";
+import { useUserStore } from "@/hooks/UserStore";
+import Faq from "./Faq";
 // import WalletConnect from "../wallet/WalletConnect";
 // import Image from 'next/image';
 
 type NavbarProps = {};
 
 const Navbar: React.FC<NavbarProps> = () => {
+  const { client } = useUserStore();
   return (
     <header className="border-b border-custom-1 w-full absolute top-0 left-0 h-16 flex items-center px-10 z-10 text-slate-100 justify-between">
       <h1 className="text-2xl font-bold md:text-4xl">Yapster</h1>
       {/* <WalletConnect /> */}
+      {client.added && <AddFrame />}
+      <div className="flex items-center gap-4">
+        <Faq />
+        <AddFrame />
+      </div>
     </header>
   );
 };
