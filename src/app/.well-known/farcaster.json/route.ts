6cc835307fb62bd8c5bf6d99f53eb6e833075835
kirtirajsinh <kirtiraj3738@gmail.com>
2025-02-21 20:01:38 +0530

    Update account association details and rename frame to 'yapyap'

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -4,14 +4,14 @@ export async function GET() {
     const roomId = process.env.NEXT_PUBLIC_ROOM_ID;
 
     const config = {
-        accountAssociation: {
-            header: "eyJmaWQiOjY4NjEsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHhBNTkzMUE3MjZDRUNjYTA1N0EzRkY0M0E4NDg4MzQ2NjI2MDQ3OEI0In0",
-            payload: "eyJkb21haW4iOiJ5YXBzdGVyLmZ1biJ9",
-            signature: "MHhiNDM1NDhiM2Q1OGQ0ZDIxZGYxMmRhYmJhYWZkZDE0ZDdlNmE2NjI5MDNiOTYzNTE4NDBiYjI3NDBkZTAwMDYyMGZmZGU0YzEzMTM2OTNhOGFmNWY2NzhiN2E2Zjk1NGIyODQ3MWUwNzY4MGFkYjNlMzYyOTUwNGY5MGRkZWY0MzFi"
+        "accountAssociation": {
+            "header": "eyJmaWQiOjY4NjEsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHhBNTkzMUE3MjZDRUNjYTA1N0EzRkY0M0E4NDg4MzQ2NjI2MDQ3OEI0In0",
+            "payload": "eyJkb21haW4iOiJ5YXB5YXAuc3BhY2UifQ",
+            "signature": "MHhiYWM1NzlkYjZlOTZiMDAxMmZjMTdhOWRiYjIzNzNkZTk3ODM0MGU4YmNhNGEyN2VjNWI4MjgxMTczZjk5NzYyMjRkNjYzMjg0YzU2MzZlMGYzZWFiMTY2OGQwMmExN2Q3MTdmMTM0YjE3YWExZjNkYjU0OTcyYTZlMTg3NjgyMjFi"
         },
         frame: {
             version: "1",
-            name: "Yapster",
+            name: "yapyap",
             iconUrl: "https://pub-b8acacbdf4c34874a29a2fdaab996f29.r2.dev/yap%20logo.png",
             homeUrl: `${appUrl}/${roomId}`,
             imageUrl: "https://pub-b8acacbdf4c34874a29a2fdaab996f29.r2.dev/main%20image.png",
