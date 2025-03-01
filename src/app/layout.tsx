ca2ced67f18adca679949dd1d456c0224aa6a489
kirtirajsinh <kirtiraj3738@gmail.com>
2025-03-01 21:59:08 +0530

    Change HTML class from "dark" to "light" in RootLayout for improved theme consistency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="dark" suppressHydrationWarning>
+    <html lang="en" className="light" suppressHydrationWarning>
       <body className={`${manrope.variable} font-manrope antialiased`}>
         <HuddleContextProvider>
           <Navbar />
