e650496bc6583004520ac08a645a6f4da28bd8bf
kirtirajsinh <kirtiraj3738@gmail.com>
2025-03-01 22:06:08 +0530

    Refactor logo handling in metadata to use environment variable for improved flexibility and consistency

--- src/app/[roomId]/layout.tsx
+++ src/app/[roomId]/layout.tsx
@@ -18,6 +18,7 @@ export async function generateMetadata({
 
   const appUrl = process.env.NEXT_PUBLIC_URL;
   const imageUrl = process.env.NEXT_PUBLIC_IMAGE_URL;
+  const logoUrl = process.env.NEXT_PUBLIC_LOGO;
 
   return {
     title: `yapyap`,
@@ -44,9 +45,8 @@ export async function generateMetadata({
             type: "launch_frame",
             name: "yapyap",
             url: `${appUrl}/${roomId}`,
-            splashImageUrl:
-              "https://pub-b8acacbdf4c34874a29a2fdaab996f29.r2.dev/yap%20logo.png",
-            splashBackgroundColor: "#000000",
+            splashImageUrl: `${logoUrl}`,
+            splashBackgroundColor: "#FFFFFF",
           },
         },
       }),
